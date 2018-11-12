855b2b80b741f99e5104b5a6e49e0cae32b95ffe
ElminaIusifova <32398063+ElminaIusifova@users.noreply.github.com>
2018-11-12 12:38:04 -0500

    Finished Lab 7

---
+++ app/src/main/java/com/example/elminaiusifova/androidlabs/WeatherForcast.kt
@@ -0,0 +1,12 @@
+package com.example.elminaiusifova.androidlabs
+
+import android.app.Activity
+import android.os.Bundle
+
+class WeatherForcast : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_forcast)
+    }
+}
